07c94fbcff6368a950d73a58c44f16a871e0c3e9
Alysson Machado <alyssonmachado388@gmail.com>
2023-01-24 10:41:58 -0300

    versão 0.1 da aplicação

---
+++ application/get_descriptions.py
@@ -0,0 +1,26 @@
+def dcgan_description():
+    
+    description = 'A arquitetura DCGAN (Deep Convolutional Generative Adversarial Network) é uma '\
+                  'técnica de aprendizado de máquina profunda para gerar imagens. Ela consiste em dois '\
+                  'componentes principais: um gerador e um discriminador. O gerador tenta criar imagens '\
+                  'que pareçam reais,enquanto o discriminador tenta identificar se as imagens geradas são '\
+                  'reais ou falsas. Esses dois componentes trabalham juntos em um processo de treinamento '\
+                  'iterativo, onde o gerador aprende a criar imagens cada vez mais realistas, e o '\
+                  'discriminador aprende a identificar essas imagens cada vez melhor. A arquitetura DCGAN '\
+                  'é baseada em Redes Neurais Convolucionais, que são tipicamente usadas para processamento '\
+                  'de imagens.'
+    
+    return description
+
+def mnist_description():
+
+    description = 'MNIST é uma base de dados de dígitos manuscritos amplamente utilizada em aprendizado '\
+                  'de máquina e visão computacional. A base de dados contém 60.000 imagens de treinamento '\
+                  'e 10.000 imagens de teste, cada uma com uma resolução de 28x28 pixels, contendo dígitos '\
+                  'de 0 a 9 escritos à mão. As imagens são brancas e pretas, onde os pixels brancos '\
+                  'representam o fundo e os pretos representam o traço do dígito. A base de dados é conhecida '\
+                  'por ser simples, porém desafiadora, e é frequentemente utilizada como benchmark para '\
+                  'algoritmos de reconhecimento de dígitos escritos à mão. A base de dados foi criada pelo '\
+                  'Yann LeCun, Corinna Cortes e Christopher J.C. Burges, que publicou o trabalho em 1998.'
+    
+    return description
